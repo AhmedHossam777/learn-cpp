e2c2e57c3c61d5e9d6662abf68691f068aead0d7
AhmedHossam777 <ahmedhossam9226@gmail.com>
2025-11-18 14:47:44 +0200

    i

--- insertion-sort/main.cpp
+++ insertion-sort/main.cpp
@@ -2,26 +2,16 @@
 using namespace std;
 
 void insertion(int arr[], int size) {
-    for (int i = 1; i < size; i++) {
-        int key = arr[i];
-        int j = i - 1;
-
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
+    for (int i = 1; i < size; ++i) {
+        for (int j = i; j > 0 && arr[j] < arr[j - 1]; --j) {
+            swap(arr[j], arr[j - 1]);
         }
-        arr[j + 1] = key;
     }
 }
 
-
 int main() {
-
     int arr[8] = {90, 3, 6, 7, 0, 1000, 60, 2};
-
     insertion(arr, 8);
-    for (int i = 0; i < 8; i++) {
-        cout << arr[i] << ' ';
-    }
+    for (int i = 0; i < 8; ++i) cout << arr[i] << ' ';
     return 0;
 }
