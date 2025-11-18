b78c7c0d5abf88b93a33a1b47d5006665c0d412d
AhmedHossam777 <ahmedhossam9226@gmail.com>
2025-11-18 14:12:45 +0200

    insertion sort

--- insertion-sort/main.cpp
+++ insertion-sort/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+using namespace std;
+
+void insertion(int arr[], int size) {
+    int lastIxd;
+    for (int i = 0; i <= size-1; ++i) {
+        lastIxd = i + 1;
+        while (lastIxd > 0) {
+            if (arr[lastIxd] < arr[lastIxd - 1]) {
+                swap(arr[lastIxd], arr[lastIxd - 1]);
+            }
+            lastIxd--;
+        }
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int arr[8] = {90, 3, 6, 7, 0, 1000, 60, 2};
+
+    insertion(arr, 8);
+    for (int i = 0; i < 8; i++) {
+        cout << arr[i] << ' ';
+    }
     return 0;
 }
