c3956ba920c4370df212bc6179ed4b59dfc40400
AhmedHossam777 <ahmedhossam9226@gmail.com>
2025-11-18 14:52:08 +0200

    i

--- insertion-sort/main.cpp
+++ insertion-sort/main.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 void insertion(int arr[], int size) {
-    for (int i = 1; i < size; ++i) {
-        for (int j = i; j > 0 && arr[j] < arr[j - 1]; --j) {
-            swap(arr[j], arr[j - 1]);
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = i + 1; j > 0; --j) {
+            if (arr[j] < arr[j - 1]) {
+                swap(arr[j], arr[j - 1]);
+            }
         }
     }
 }
