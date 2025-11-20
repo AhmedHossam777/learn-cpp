67da2a54495b2e9a9c0ba06e9cc2586908a8708c
AhmedHossam777 <ahmedhossam9226@gmail.com>
2025-11-20 13:48:21 +0200

    add stack to linked list

--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -53,6 +53,10 @@ public:
         return this->pStart;
     }
 
+    Employee *setStart(Employee *_pStart) {
+        pStart = _pStart;
+    }
+
     Employee *getEnd() {
         return this->pEnd;
     }
@@ -63,6 +67,14 @@ public:
 
     Employee *getNext() {
         return this->pNext;
+    }
+
+    Employee *getlast() {
+        Employee *emp;
+        while (emp->pNext) {
+            emp = emp->pNext;
+        }
+        return emp;
     }
 
     void addEmp(Employee *pItem) {
@@ -122,27 +134,57 @@ public:
     //     Employee *pItem;
     //     pItem = pStart;
     //
-    //     Employee* biggerPrev = pItem->pPrev;
-    //     Employee* biggerNext = pItem->pNext;
+    //     Employee *biggerPrev = pItem->pPrev;
+    //     Employee *biggerNext = pItem->pNext;
     //
-    //     Employee* smallerPrev = pItem->pNext->pPrev;
-    //     Employee* smallerNext = pItem->pNext->pNext;
+    //     Employee *smallerPrev = pItem->pNext->pPrev;
+    //     Employee *smallerNext = pItem->pNext->pNext;
     //
-    //     Employee* smaller = pItem->pNext;
+    //     Employee *smaller = pItem->pNext;
     //     while (pItem) {
     //         while (pItem) {
     //             if (pItem->getCode() > pItem->pNext->getCode()) {
     //                 pItem->pNext = smallerNext;
     //                 pItem->pPrev = smallerPrev;
     //
-    //                 pItem->pNext->pNext =
-    //
+    //                 // pItem->pNext->pNext =
     //             }
-    //         pItem = pItem->pNext;
+    //             pItem = pItem->pNext;
     //         }
     //         pItem = pItem->pNext;
     //     }
     // }
+};
+
+
+class Stack : private LinkedList {
+public:
+    Stack() : LinkedList() {
+    }
+
+    void push(Employee *emp) {
+        addEmp(emp);
+    }
+
+    Employee *pop() {
+        if (!isEmpty()) {
+            Employee* emp = pEnd;
+            emp = emp->pPrev;
+            emp->pNext = nullptr;
+            return emp;
+        }
+    }
+
+    void displayAll() {
+        print();
+    }
+
+    Employee *findEmp(int code) {
+        return search(code);
+    }
+
+    ~Stack() {
+    }
 };
 
 int binarySearch(Employee emps[], int len, int code) {
@@ -169,20 +211,34 @@ int main() {
     Employee emp4(1000, 4, "ahmed hossam");
     LinkedList l1;
 
-    l1.addEmp(&emp1);
-    l1.addEmp(&emp2);
-    l1.addEmp(&emp3);
-    l1.insertEmp(&emp4, 2);
+    Stack s1;
+    s1.push(&emp1);
+    s1.push(&emp2);
+    s1.push(&emp3);
+    s1.push(&emp4);
+
+    cout << "stack result after push: " << endl;
+    s1.displayAll();
+
+    cout << "stack result after pop: " << endl;
+    s1.pop();
+    // cout << "the poped element: " << poped << endl;
+    s1.displayAll();
+
+    // l1.addEmp(&emp1);
+    // l1.addEmp(&emp2);
+    // l1.addEmp(&emp3);
+    // l1.insertEmp(&emp4, 2);
     // l1.print();
 
 
     // l1.sort();
-    l1.print();
+    // l1.print();
 
-    Employee employees[] = {emp1, emp2, emp3, emp4};
-    int resultIdx = binarySearch(employees, 4, 4);
-
-    cout << "the result of binary search: " << resultIdx << endl;
+    // Employee employees[] = {emp1, emp2, emp3, emp4};
+    // int resultIdx = binarySearch(employees, 4, 4);
+    //
+    // cout << "the result of binary search: " << resultIdx << endl;
 
 
     // Employee *empS = l1.search(3);
